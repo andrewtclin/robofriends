beb158332ec3ca604b894001707b0456748d42e4
Andrew <chuntcdj@gmail.com>
2020-08-12 10:44:06 +0800

    inital commit

---
+++ src/components/CardList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((robot, index) => {
+    return (
+      <Card
+        key={robots[index].id}
+        id={robots[index].id}
+        name={robots[index].name}
+        email={robots[index].email}
+      />
+    );
+  });
+  return <div>{cardComponent}</div>;
+};
+
+export default CardList;
